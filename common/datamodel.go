a7b7917ac0fc84bd05a09d8e80f59948ac5f0ebb
AmiR <rahafrouz@gmail.com>
2019-08-05 20:45:23 +0200

    Moved random Token generator to common to be reusable.

--- common/datamodel.go
+++ common/datamodel.go
@@ -1,5 +1,10 @@
 package datamodel
 
+import (
+	"crypto/rand"
+	"fmt"
+)
+
 //Data Structure of the message envelope for sending request
 type SSRequest struct {
 	URL string
@@ -8,4 +13,10 @@ type SSRequest struct {
 type SSResponse struct {
 	Result    bool
 	ImagePath string
+}
+
+func TokenGenerator(size int) string {
+	b := make([]byte, size)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
 }
