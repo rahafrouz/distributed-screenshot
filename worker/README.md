dff5c827f8a9a8f11e2e02e7ccfabe00ba155793
AmiR <rahafrouz@gmail.com>
2019-08-07 05:53:58 +0200

    Cleanup the worker code

--- worker/README.md
+++ worker/README.md
@@ -1,15 +1,26 @@
 # The System Architecture
 
+## Run it
+To run, first edit .env file and enter the credentials. Then enter `docker-compose up`. Voila!
+
+## Alternatives
 There are different engines for taking screenshot. 
-Here we use the package called `gowitness`. 
+Here we use the package called `chromedp`. 
 It uses `chrome-headless` as the internal engine. 
 
 Other alternatives are:
 - **eyewitness**: It is an option. It _seems_ they use firefox engine.  
 - **chrome-headless**: For this package, it seems the emphasis is on the debug protocol for comprehensive commands. 
-It seems promising, however, using the command line, there was no way to change the output of the file (It is screenshot.png). _There is a way, but I could not find it._ TODO For Future.   
+It seems promising, however, using the command line, there was no way to change the output of the file (It is screenshot.png).
+ _There is a way, but I could not find it._ TODO For Future.   
 - **PhantomJS**: It utilized the debug protocol of `chrome headless`. It seems a more mature option.
+- **ChromeDP**: This is the current implementation. The API is from chrome developer team, first-hand, and does not have any intermediary such as gowitness (Which has problems in concurrency) It has Golang API to call to take screenshot. 
+Nice to use as a library, while for others such as `gowitness`, you would have to call OS-level command to run it, which gives you less flexibility. 
+This engine can have extra options such as resolution, vieport, ... for future development.
+#### Extentding the functionality
+Right now, the `chromedp` engine is implemented. To use another engine, look at `common/utils.go`. Create a data structure that agrees to ` ScreenshotHanlder ` interface.
 
-The option of gowintess, is not the best, however, it does the job for current requirement. 
-To replace it in the future, just the file `worker.go` should be modified. 
+# Build Instructions.
+To build the worker, refer to `build` file. Most of the options are feeded into the worker from the environment variables. The `docker-compose` is used for deployent. 
 
+*Currently, an executalbe for linux architecture is generated, and added to a container. The container contains chrome.*
--- worker/worker.go
+++ worker/worker.go
@@ -6,7 +6,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/rahafrouz/distributed-screenshot/common"
 	"github.com/streadway/amqp"
-	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
@@ -32,31 +31,20 @@ func failOnError(err error, msg string) {
 
 func main() {
 	w := Worker{}
-	//w.TakeScreenshot("http://www.google.com",".")
 	w.InitAndListen()
 
 }
 
-//func (w *Worker) InitS3() {
-//	////select Region to use.
-//	//conf := aws.Config{
-//	//	Region:      aws.String(os.Getenv("AWS_REGION")),
-//	//	Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), ""),
-//	//}
-//	//conf.WithCredentialsChainVerboseErrors(true)
-//	//sess, err := session.NewSession(&conf)
-//	//failOnError(err, "Problem in setting aws session")
-//	//w.svc = s3manager.NewUploader(sess)
-//}
 func (w *Worker) InitAndListen() {
-	//w.screenshotHandler = datamodel.GowitnessScreenshotHandler{}
+	//Setting up engines for screenshot and storage
 	w.screenshotHandler = &common.ChromeDPScreenshotHandler{}
 	w.storageHandler = &common.S3Storage{}
-	//w.InitS3()
-	fmt.Printf("Initialized the worker")
+	log.Printf("Initialized the worker")
 
-	w.GOWITNESS_PATH = os.Getenv("GOWITNESS_PATH")
+	//Not for current implementation
+	//w.GOWITNESS_PATH = os.Getenv("GOWITNESS_PATH")
 
+	//Connect to rabbitMQ Broker
 	ConnectionString := fmt.Sprintf("amqp://%s:%s@%s:%s",
 		os.Getenv("RMQ_USER"),
 		os.Getenv("RMQ_PASS"),
@@ -72,6 +60,7 @@ func (w *Worker) InitAndListen() {
 	failOnError(err, "Failed to open a channel")
 	defer w.ch.Close()
 
+	//This is the queue of tasks. We take jobs from here to process.
 	w.q, err = w.ch.QueueDeclare(
 		"task_queue", // name
 		true,         // durable
@@ -81,6 +70,8 @@ func (w *Worker) InitAndListen() {
 		nil,          // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
+
+	//Number of active screenshot-taking jobs to handle in parallel. It should be tuned by experiment to utilize the hardware.
 	workerThreadSize, _ := strconv.Atoi(os.Getenv("WORKER_THREADS"))
 	err = w.ch.Qos(
 		workerThreadSize, // prefetch count
@@ -88,6 +79,8 @@ func (w *Worker) InitAndListen() {
 		false,            // global
 	)
 	failOnError(err, "Failed to set QoS")
+
+	//Consume tasks from the task_queue.
 	msgs, err := w.ch.Consume(
 		"task_queue", // queue
 		"",           // consumer
@@ -101,6 +94,7 @@ func (w *Worker) InitAndListen() {
 
 	forever := make(chan bool)
 
+	//main loop of worker. Listen for jobs.
 	go func() {
 		for d := range msgs {
 			go func() {
@@ -121,9 +115,12 @@ func (w *Worker) InitAndListen() {
 	<-forever
 
 }
+
+//Publish the result to the callback_queue. Using the correlation_id, the sender would identify it, and can get it.
 func (w Worker) PublishResponse(resultLocation string, d amqp.Delivery) error {
 	log.Println("Finished taking screenshot. Sending back the response")
 
+	//building the response envelope
 	responseMsg := common.SSResponse{
 		Result:    true,
 		ImagePath: resultLocation,
@@ -148,67 +145,6 @@ func (w Worker) PublishResponse(resultLocation string, d amqp.Delivery) error {
 	return err
 }
 
-//func (w *Worker) TakeScreenshot(u string, dest string) bool {
-//
-//
-//	//utils.ProcessURL(url.URL{})
-//	fmt.Println("Taking Screenshot")
-//	//args := fmt.Sprintf("--headless --disable-gpu --screenshot %s --no-sandbox",url)
-//	//args := fmt.Sprintf(" single --url=%s --destination=%s",url,dest)
-//	cmd := exec.Command(os.Getenv(
-//		"GOWITNESS_PATH"),
-//		"single", "--url="+url, "--destination="+dest,
-//		"--chrome-path=/usr/bin/chromium-browser",
-//	)
-//	cmd.Stdout = os.Stdout
-//	cmd.Stderr = os.Stderr
-//	log.Printf("Running command and waiting for it to finish...")
-//	err := cmd.Run()
-//	if err != nil {
-//		return false
-//	}
-//	log.Printf("Command finished with error: %v", err)
-//	return true
-//
-//	//chromium-browser --headless --disable-gpu --screenshot https://www.chromestatus.com/ --no-sandbox
-//}
-
-//func makeDir(path string) error {
-//	_, err := os.Stat(path)
-//
-//	if os.IsNotExist(err) {
-//		errDir := os.MkdirAll(path, 0777)
-//		if errDir != nil {
-//			return errDir
-//			log.Fatal(err)
-//		}
-//	}
-//	log.Println("Created Directory", path)
-//	return nil
-//}
-
-func (w *Worker) findFileinDir(path string) string {
-	// A very dangerous way of doing it! TODO: Refactor
-	w.readDirMux.Lock()
-	//for {
-	files, err := ioutil.ReadDir(path)
-	if err != nil {
-		failOnError(err, "cannot open folder on: "+path)
-	}
-
-	w.readDirMux.Unlock()
-
-	fmt.Println("Number of files in this dir:", path, len(files))
-	if len(files) > 1 {
-		failOnError(nil, "There are more than one files in the job folder. What happened?")
-	}
-	for _, f := range files {
-		return f.Name()
-		//fmt.Println(f.Name())
-	}
-	return ""
-}
-
 //Takes the request, build the screenshot as a byte[], and then upload the array to s3.
 //No disk Operation involved with this type of handling.
 func (w *Worker) handleJobRequest(d amqp.Delivery) (string, error) {
@@ -219,7 +155,7 @@ func (w *Worker) handleJobRequest(d amqp.Delivery) (string, error) {
 	err := json.Unmarshal(d.Body, &msg)
 	failOnError(err, "unable to unmarshal message")
 
-	//TODO Here we can pass other options{resolution, viewpoint, etc} for taking screenshot
+	//Here we can pass other options{resolution, viewpoint, etc} for taking screenshot
 	//Take the screenshot!
 	screenshotData, _ := w.screenshotHandler.TakeScreenshot(msg.URL, "", false)
 
@@ -231,58 +167,3 @@ func (w *Worker) handleJobRequest(d amqp.Delivery) (string, error) {
 
 	return w.storageHandler.UploadDataToCloud(magicName, screenshotData)
 }
-
-//DEPRECATED-- JUST PUT IT HERE IN CASE OF REQUIREMENT CHANGE
-//func (w *Worker) dONTUSEITlegacyhandleJobRequest(d amqp.Delivery) (string, error) {
-//
-//	log.Printf("Received a message: %s", d.Body)
-//	msg := common.SSRequest{}
-//	err := json.Unmarshal(d.Body, &msg)
-//	failOnError(err, "unable to unmarshal message")
-//
-//	log.Println("Check if it a request for fresh one, or cache is allowed; Would take screenshot or send cache")
-//
-//	//Generate the destination path
-//	b := make([]byte, 25)
-//	rand.Read(b)
-//	MagicPath := fmt.Sprintf("%x", b)
-//
-//	//MagicPath := datamodel.TokenGenerator(25)
-//
-//	//Create Directory
-//	failOnError(makeDir(MagicPath), "Unable to create directory")
-//
-//	//Save Screenshot in the MagicPath
-//	BasePath := "/usr/src/app/"
-//	log.Println("Path to save screenshot: %s", BasePath+MagicPath)
-//
-//	screenshotData,_ := w.screenshotHandler.TakeScreenshot(msg.URL, BasePath+MagicPath,false)
-//
-//	//w.TakeScreenshot(msg.URL, BasePath+MagicPath)
-//	//w.TakeScreenshot2(msg.URL,BasePath+MagicPath)
-//
-//	filename := BasePath + MagicPath + "/" + w.findFileinDir(BasePath+MagicPath+"/")
-//
-//	file, err := os.Open(filename)
-//
-//	failOnError(err, "Failed to open file "+filename)
-//
-//	defer file.Close()
-//	uploadpath:= MagicPath+".png"
-//
-//	//w.storageHandler.UploadToCloud(uploadpath,"",screenshotData)
-//
-//	//fmt.Println("Uploading file to S3...", filename)
-//	//result, err := w.svc.Upload(&s3manager.UploadInput{
-//	//	Bucket: aws.String(bucket),
-//	//	Key:    aws.String(filepath.Base(filename)),
-//	//	//Body:   file,
-//	//	Body: bytes.NewReader(screenshotData),
-//	//})
-//	//
-//	//failOnError(err, "Error Uploading to S3")
-//	////Everything was fine so far
-//	//
-//	//fmt.Println("Result is: " + result.Location)
-//	//return result.Location, nil
-//}
